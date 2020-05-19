62da2bf4dea10cbea879980f3bc7a47764ab79c4
Yago Juan <48865087+YagoYJ@users.noreply.github.com>
2020-05-19 14:53:31 -0300

    Identify if the post was liked

--- instagramLikes.js
+++ instagramLikes.js
@@ -4,14 +4,20 @@ function likePhoto() {
   setTimeout(() => {
     let likeButton = document.querySelector(".wpO6b");
     let nextButton = document.querySelector("._65Bje");
+    let likeButtonSvg = document.querySelectorAll("._8-yf5")[4];
+    svgText = likeButtonSvg.getAttribute("aria-label");
+    console.log(svgText);
+
+    if (svgText == "Curtir") {
+      setTimeout(() => {
+        likeButton.click();
+        console.log("Liked");
+      }, 2000);
+    }
 
     setTimeout(() => {
-      likeButton.click();
-      console.log("Curtiu");
-    }, 2000);
-    setTimeout(() => {
       nextButton.click();
-      console.log("Passou");
+      console.log("Next");
     }, 4000);
   }, 5000);
   setTimeout(() => {
